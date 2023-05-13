b166644fe1dd349948fae858be8a80f0e7a5aeec
Caio Ferreira <cs1919328@gmail.com>
2023-05-12 21:57:48 -0300

    visual em andamento

--- cursoDns.js
+++ cursoDns.js
@@ -1,5 +1,20 @@
-var menu = document.querySelector('.chamaMenu')
+//fazendo o carrossel infinito
 
-menu.addEventListener('click', () => {
+const imgs = document.querySelector('#img')
+const img = document.querySelectorAll('#img img')
 
-})
+let idx = 0
+
+function carrossel() {
+    idx++
+
+    if(idx > img.length - 1) {
+        idx = 0
+    }
+
+    imgs.style.transform = `translateX(${-idx * 380}px)`
+}
+
+setInterval(carrossel, 4000)
+
+//
